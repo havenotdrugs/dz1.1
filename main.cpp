48df21c13e20ce097289d2c6a2c16823e3aab229
havenotdrugs <132453806+havenotdrugs@users.noreply.github.com>
2023-05-03 14:15:44 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//Задание 1.1
+int main() {
+  int a;
+  cout << "Привет. Я возвожу в квадрат заданное тобой число.\nВведи число: ";
+  cin >> a;
+  cout << "Твое число в квадрате - " << a * a;
+  return 0;
+}
